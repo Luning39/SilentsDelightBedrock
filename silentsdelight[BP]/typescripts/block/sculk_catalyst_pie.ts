55fe804859ce08914883bc5910dd52f1d3161f86
Luning39 <luning12418@qq.com>
2026-05-22 23:10:47 +0800

    work done half

--- silentsdelight[BP]/typescripts/block/sculk_catalyst_pie.ts
+++
@@ -1,7 +0,0 @@
-import {
-    Block, Vector3, Player, world 
-} from @minecraft/server;
-import { EventAPI } from "../lib/EventAPI";
-
-export class sculkCatalystPie {
-    @EventAPI.register(world.afterEvents.entityDie)
---
+++ silentsdelight[BP]/typescripts/customComponents/blocks/sculk_catalyst_pie.ts
@@ -0,0 +1,5 @@
+import {Block, Vector3, Player, world, BlockCustomComponent } from @minecraft/server;
+import { EventAPI } from "../../lib/EventAPI";
+
+export class sculkCatalystPie {
+    @EventAPI.register(world.afterEvents.entityDie)
